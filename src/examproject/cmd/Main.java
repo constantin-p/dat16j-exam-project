32b3f57d865ca9113f4098b13d9c1fd2171fe75a
constantin-p <constantin.panaitescu@yahoo.com>
2016-11-15 09:26:07 +0100

    add basic structure

---
+++ src/examproject/cmd/Main.java
@@ -0,0 +1,11 @@
+package examproject.cmd;
+
+import examproject.core.PlaceholderFunctionalityProvider;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test from cmd");
+        PlaceholderFunctionalityProvider.testPrint();
+    }
+}
---
+++ src/examproject/core/PlaceholderFunctionalityProvider.java
@@ -0,0 +1,7 @@
+package examproject.core;
+
+public class PlaceholderFunctionalityProvider {
+    public static void testPrint() {
+        System.out.println("Test from core");
+    }
+}
