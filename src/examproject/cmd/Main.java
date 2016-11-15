4be503fda8fe834e1fcb19f46260b7fd18dbd321
constantin-p <constantin.panaitescu@yahoo.com>
2016-11-15 14:53:52 +0100

    add ScreenManager class & main views

--- src/examproject/cmd/Main.java
+++ src/examproject/cmd/Main.java
@@ -1,11 +1,98 @@
 package examproject.cmd;
 
-import examproject.core.PlaceholderFunctionalityProvider;
+import java.util.ArrayList;
 
 public class Main {
 
+    private static ScreenManager screenManager = new ScreenManager();
+
     public static void main(String[] args) {
-        System.out.println("Test from cmd");
-        PlaceholderFunctionalityProvider.testPrint();
+        showMainMenu();
+    }
+
+    private static void showMainMenu() {
+        ArrayList<String> mainMenu = new ArrayList<String>();
+        mainMenu.add("Chairman login.");
+        mainMenu.add("Treasurer login.");
+
+        mainMenu.add("Exit.");
+
+        int selectedOption = screenManager.getOptionsView(" - Main menu - ", mainMenu);
+        switch (selectedOption) {
+            case 0:
+                // Chairman login option
+                showChairmanLogin();
+                break;
+            case 1:
+                // Treasurer login option
+                showTreasurerLogin();
+                break;
+            case 2:
+                // Exit option
+                break;
+        }
+    }
+
+    /*
+     *  Chairman views
+     */
+    private static void showChairmanLogin() {
+        String username = screenManager.getStringInputView(" - [Chairman] username: - ", 4, 10);
+        String password = screenManager.getStringInputView(" - [Chairman] password: - ", 4, 10);
+
+        // TODO: Validation
+
+        showChairmanMenu();
+    }
+
+    private static void showChairmanMenu() {
+        ArrayList<String> chairmanMenu = new ArrayList<String>();
+        chairmanMenu.add("Add new member.");
+
+        chairmanMenu.add("Log out (back to main menu).");
+
+        int selectedOption = screenManager.getOptionsView(" - Chairman menu - ", chairmanMenu);
+        switch (selectedOption) {
+            case 0:
+                // Add new member option
+                System.out.println("\nNot yet implemented!");
+                break;
+            case 1:
+                // Back to main menu option
+                showMainMenu();
+                break;
+        }
+    }
+
+    /*
+     *  Treasurer views
+     */
+    private static void showTreasurerLogin() {
+        String username = screenManager.getStringInputView(" - [Treasurer] username: - ", 4, 10);
+        String password = screenManager.getStringInputView(" - [Treasurer] password: - ", 4, 10);
+
+        // TODO: Validation
+
+        showTreasurerMenu();
+    }
+
+
+    private static void showTreasurerMenu() {
+        ArrayList<String> treasurerMenu = new ArrayList<String>();
+        treasurerMenu.add("Show members.");
+
+        treasurerMenu.add("Log out (back to main menu).");
+
+        int selectedOption = screenManager.getOptionsView(" - Treasurer menu - ", treasurerMenu);
+        switch (selectedOption) {
+            case 0:
+                // Show members option
+                System.out.println("\nNot yet implemented!");
+                break;
+            case 1:
+                // Back to main menu option
+                showMainMenu();
+                break;
+        }
     }
 }
