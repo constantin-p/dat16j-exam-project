70ff551339c288d3008119c1a7c6a578ff6f7952
Magnus S. Jakobsson <maagiij@gmail.com>
2016-11-17 10:01:02 +0100

    add member constructor

--- src/examproject/core/Member.java
+++ src/examproject/core/Member.java
@@ -8,5 +8,10 @@ public class Member {
     public String lastName;
     public String preferredActivity;
     public Date dateOfBirth;
-    
+
+    public Member(String firstName, String lastName, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
 }
