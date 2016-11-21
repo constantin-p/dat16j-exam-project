e3c2f9db02c553f0949e0c3b22097d9f2da3ef20
Felix <felixmanta10@gmail.com>
2016-11-21 11:27:36 +0100

    save discount functionality

--- src/examproject/cmd/Main.java
+++ src/examproject/cmd/Main.java
@@ -8,6 +8,7 @@ import examproject.core.SeniorDiscount;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
 
@@ -142,7 +143,13 @@ public class Main {
         // loop throw all the members and create a string for the option label
         for(int i = 0; i < members.size(); i++) {
             Member currentMember = members.get(i);
-            options.add(currentMember.firstName + " " + currentMember.lastName + " " + currentMember.cprNumber);
+            String currentMemberDicounts = currentMember.getAppliedDiscountsString();
+
+            if(Objects.equals(currentMemberDicounts, "")) {
+                options.add(currentMember.firstName + " " + currentMember.lastName + " " + currentMember.cprNumber + " No discounts applied for this account.");
+            } else {
+                options.add(currentMember.firstName + " " + currentMember.lastName + " " + currentMember.cprNumber + " discounts: " + currentMemberDicounts);
+            }
         }
 
         int selectedMemberIndex = screenManager.setOptionsView(" - Member list - ", options);
@@ -187,5 +194,6 @@ public class Main {
         int selectedDiscount = screenManager.setOptionsView(" - Discount list - ", options);
         System.out.println(selectedDiscount);
         // TODO: apply the discount (save it to the selected member)
+        member.applyDiscount(discounts.get(selectedDiscount));
     }
 }
